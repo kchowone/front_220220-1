f52cb866bf301ddd2e88886052cc86263d71f295
kchowone <chtlrekd@gmail.com>
2022-04-24 17:56:56 +0900

    [Fix]:ab:js_typeCheck,operation

---
+++ code/step_003/js/src/a_03_js_typeCheck.js
@@ -0,0 +1,112 @@
+'use strict'
+// a_03_js_typwCheck.js
+
+let num = 10;
+let likeNum = '10';
+
+let checkType;
+
+checkType = typeof(num);
+console.log(num +':' + checkType);
+
+checkType = typeof likeNum;
+console.log(likeNum + ':' + checkType);
+
+let bool = true;
+checkType = typeof(bool)
+console.log(bool + ':' + checkType);
+
+let check = 0 !== '0';
+console.log(bool);
+
+let num2;
+console.log(!!num2);
+
+console.clear()
+
+//----------------------------------------------
+/*
+let confirmText  = '나이를 입력하세요.';
+let inputMessage = prompt(confirmText);
+let covertNumber = parseInt(inputMessage)
+console.log(covertNumber)
+
+if(!!convertNumber){
+  console.log('입력한 숫자는 0이 아닌숫자')
+}
+
+if(convertNumber < 20 ){
+  console.log('현재 나이는 20세 이하');
+}
+*/
+
+//-------------------------------------------------
+
+let variable;
+checkType = typeof variable;
+console.log(checkType);
+checkType = undefined;
+console.log(checkType);
+
+let nothing = 10;
+console.log(nothing);
+nothing = null;
+console.log(nothing);
+
+checkType = typeof nothing;
+console.log(checkType)
+
+// #wrap{width:100px; height:100px;}
+// <div class="box" title="hi"></div>
+// null = object
+
+// JS 형타입 / 자료구조
+/*
+원시형태 (기본형) : 숫자(number),문자(string),논리(boolean),undefined, null, NaN , bigInt, symbol
+특수형태 : object(object, array), function
+*/
+
+let ch = NaN;
+checkType = typeof ch;
+console.log(checkType)  //숫자로 뜬다
+
+//-------------------------------------------------
+console.clear();
+// Object - > Object
+//객체형태
+let obj = {'apple':'red' ,'banana':'yellow', 'grape':'purple' ,'파인 애플':'갈색'};
+console.log(obj);
+console.log(obj.apple);
+console.log(obj['파인 애플']);
+
+//배열형태
+let arr = ['apple', 'banana', 'grape', '파인 애플']; //중가로{}는 무조건 값이 있어야한다 값이 없을때는 대가로[]로 표현
+console.log(arr);
+console.log(arr[0]);
+
+let gndlist = [
+  {'name' : 'about us' , 'href' : '#'},
+  {'name' : 'product' , 'href' : '#'}, 
+  {'name' : 'gallery' , 'href' : '#'}, 
+  {'name' : 'notice' , 'href' : '#'}, 
+  {'name' : 'event' , 'href' : '#'}, 
+  {'name' : 'sns' , 'href' : '#'} 
+
+];
+
+for(let i=0;i < gndlist.length; i = i + 1){
+//console.log(arr[i])
+console.log(gndlist[i].name);
+}
+
+
+// let data = ['pen', 'cup' , 'mic']; // //data[2]
+// let data2 = {'pen':'red', 'cup' : 'white', 'mic': 'black'}; //data2['mic'] || data2.mic
+
+// let data3 = [
+//   {'pen':'red', 'cup' : 'white', 'mic': 'black'}, 
+//   {'pen':'red', 'cup' : 'white', 'mic': 'gray'},
+//   {'pen':'red', 'cup' : 'white', 'mic': 'black'}
+// ];
+// //data3[2].mic
+// //data3[1].[mic]
---
+++ code/step_003/js/src/a_04_js_operation.js
@@ -0,0 +1,70 @@
+'use strict';
+//a_04_js_operation.js
+
+// operator(계산자,연산자), literal( 변수로 된 값)
+let num, num2;
+
+num = 10;
+num2 = 20;
+
+[num,num2] = [30,40];
+console.log(num2);
+
+// 사칙연산 : + - * / %
+
+let rel1 = num + num2;
+console.log( rel1 ); // 70
+
+rel1 = rel1 + 10;
+console.log( rel1 ); //80
+
+//나는 나 자신에게 10을 더할거야!
+//ㄴ 나에게 10을 더할거야!
+// rel1 = rel1 + 10;
+rel1 += 10;
+console.log(rel1);//90
+
+rel1 -= 5;
+console.log(rel1);//85
+
+rel1 *= 20;
+console.log(rel1); //1700
+
+rel1 /= 15;
+rel1= parseInt(rel1); //정수 :parseInt
+console.log(rel1);
+
+let rel2 = rel1 % 10;
+console.log (rel2);
+
+//rel1에게 10으로 나누었을때 몫 필요없고, 나머지 값은?
+rel1 %=10; 
+console.log(rel1);
+
+//-----------------------------
+let rel3 = num2;
+console.log( rel3 );
+
+rel3 += 1;
+console.log( rel3 );
+
+rel3++;//후치, 증가연산
+console.log( rel3 );
+++rel3;//전치, 증가연산
+console.log( rel3 );//43
+
+console.log( ++rel3 );
+console.log( ++rel3 );
+console.log( rel3++ );
+console.log( rel3++ );
+console.log( rel3 );
+
+rel3--;
+--rel3;
+console.log(rel3);
+
+//=, ===, !, !== , <, <= , >, >= , ++ , -- , += , -= ..... (복합연산자)
+// && , || (비교연산자)
+// 조건문 : if, switch , 삼항연산자
+// 반복문 : for, for-in , forEach , while, do-while
+
