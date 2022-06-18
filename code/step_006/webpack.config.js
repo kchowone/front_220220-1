86bfc2735868e9af652bda81cb2ddcd30113450c
Your Name <you@example.com>
2022-06-18 16:45:23 +0900

    [Feat] node test

---
+++ code/step_006/webpack.config.js
@@ -0,0 +1,14 @@
+// webpack.config.js
+const path = require('path');
+
+
+module.exports ={
+  mode : 'development',
+  entry : {
+    main : './js/src/use.js'
+  },
+  output : {
+    path : path.resolve('./dist'),
+    filename : '[name].js' 
+  }
+}
