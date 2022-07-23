0e92c3151680425adcb97903914a8413109efe34
Your Name <you@example.com>
2022-07-23 17:59:14 +0900

    [Fix]:ab:react_route

--- code/step_008_route/src/Pages/ProductContent.js
+++ code/step_008_route/src/Pages/ProductContent.js
@@ -14,7 +14,7 @@ function ProductContent() {
         <p>1. Lorem ipsum dolor sit amet.</p>
         <div className='link'>
           <Link to='/about'>about 페이지 이동</Link>
-          <button type='button' onClick={()=> navigate('product/about') }>상세 내용 보기</button>
+          <button type='button' onClick={()=> navigate('about') }>상세 내용 보기</button>
         </div>
       </li>
       <li>
